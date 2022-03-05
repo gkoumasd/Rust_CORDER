2ea611c0e846c1162439d46c62e9de4525718476
Yijun Yu <yijun.yu@huawei.com>
2022-03-06 01:57:40 +0800

    update the tree-sitter grammar

--- tokens.sh
+++ tokens.sh
@@ -1,4 +1,5 @@
-find data -name "*.asm" | while read f; do
+find treesitter_asm_train_test_val/ -name "*.asm" | while read f; do
 #   tree-sitter parse -q $f
-    tree-grepper -q asm '(operand) @op' $f | cut -d: -f5
+#    tree-grepper -q asm '(operand) @op' $f | cut -d: -f5
+	~/Documents/github.com/yijunyu/tree-sitter-parsers/tree-sitter-asm-0.0.1/tokens.sh $f
 done | sort | uniq > vocab/treesitter_asm/node_token/token.txt
---
+++ types.sh
@@ -0,0 +1,2 @@
+~/Documents/github.com/yijunyu/tree-sitter-parsers/tree-sitter-asm-0.0.1/types.sh
+mv types.txt vocab/treesitter_asm/node_type/type.txt
